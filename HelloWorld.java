594d049e505cdc8d00521ad4c9b9910128b4c5df
aidansundt <aidansundt@gmail.com>
2024-01-17 17:44:27 -0500

    Added code for my initial in ASCII

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,20 @@
 //HelloWorld.java
-
+// Programmers:  Aidan Sundt
+// Course:  CS 212, Mr. John
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initial of the programmer in ASCII art form
+// Credits: N/A
 class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-        
+        System.out.println("    _");
+        System.out.println("   / \\");
+        System.out.println("  / _ \\");
+        System.out.println(" / ___ \\");
+        System.out.println("/_/   \\_\\");
     }
 }
